e53b63a429800ef846fafe2e96af7894e13ca6b6
Gauthier Bosson <bosson.gauthier27@gmail.com>
2024-08-23 15:14:20 +0200

    added basic metadata

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,8 +1,21 @@
 import { type SEOHandle } from '@nasa-gcn/remix-seo';
+import { type MetaFunction } from '@remix-run/react';
 import { serverOnly$ } from 'vite-env-only/macros';
 import { routesConfig } from '../utils/routesConfig';
 
 export { default } from '../old-app/app';
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: 'Old App',
+    },
+    {
+      name: 'description',
+      content: 'This is the old app',
+    }
+  ];
+};
 
 export const handle: SEOHandle = {
   getSitemapEntries: serverOnly$(async () => {
